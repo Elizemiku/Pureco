6f0e71680830bec9380054fe35bd60efc9f9f9f2
elizemiku <e170409@dac.unicamp.br>
2020-09-25 21:44:41 -0300

    testes

--- Secoes.R
+++ Secoes.R
@@ -21,60 +21,33 @@ library(dygraphs)
 library(htmltools)
 library(readxl)
 
-source("server.R")
-source("Tabs.R")
-source("ui.R")
+source("faxinas.R")
 source("Temas.R")
 
 
-## por enquanto nao sei ainda se vou fazer um module 
-## funcao das seções 
-# histogramServer <- function(id) {
-#   moduleServer(id, function(input, output, session) {
-#     data <- reactive(mtcars[[input$var]])
-#     output$hist <- renderPlot({
-#       hist(data(), breaks = input$bins, main = input$var)
-#     }, res = 96)
-#   })
-# }
-# 
-# faxinasgeraisServer <- function(id) {
-#   moduleServer(id, function(input, output, session, faxinas){ 
-#     
-#     observeEvent(input$escolhido, {
-#       if (input$grafico == "Barras") {
-#         if (input$eixo_y == "Quantidade" & input$eixo_x == "Dia da Semana") {
-#           faxinas_g <- reactive(
-#             faxinas %>%
-#               mutate(Quantidade = 1, ano = year(Data)) %>%
-#               filter(Mulher != "NA" & `Dia da Semana` != "NA" &
-#                        ano %in% input$ano) %>%
-#               group_by(ano, `Dia da Semana`)  %>%
-#               summarize(Quantidade = sum(Quantidade)) %>%
-#               arrange(ano, Quantidade)
-#           )
-#           output$infgeral1parte1 <- renderPlotly({
-#             g1a <- ggplot(faxinas_g(),
-#                    aes(
-#                      x = `Dia da Semana`,
-#                      y = Quantidade,
-#                      fill = `Dia da Semana`
-#                    )) +
-#               geom_bar(stat = "identity", position = "stack") +
-#               facet_grid(~ ano, scales = "free_x") +
-#               xlab("Dia da Semana") +
-#               ylab("Quantidade de Faxinas") +
-#               ggtitle("Quantidade de Faxinas por Dia da Semana e Ano") +
-#               scale_fill_viridis_d(aesthetics = "fill") +
-#               tema_facets
-# 
-#             g1a <- ggplotly(g1a) %>%
-#               layout(showlegend = FALSE)
-# 
-#             g1a
-#           })
-#         }
-#       }
-#     })
-#   })
-# }
+## funcoes de graficos da secao1
+
+# barplot_secao1
+
+
+# lineplot_secao1
+
+# boxplot
+boxplot_secao1 <- function(dados, eixo_x, eixo_y, grupo){
+  ggplot(dados %>% summarize(eixo_y = cumsum(eixo_y)),
+         aes(x = .data[[eixo_x]], y = .data[[eixo_y]], 
+                   fill = .data[[grupo]])) +
+    geom_boxplot() +
+    facet_grid(~ano) + 
+    labs(x = paste0(input$eixo_x), 
+         y = paste0(input$eixo_y, " de Faxinas", sep = " ", collapse = " "), 
+         title = paste0(input$eixo_y, " de Faxinas por ", 
+                        input$eixo_x, " e Ano",
+                        sep = " ", collapse = " "))  +
+    scale_fill_viridis_d() +
+    tema_facets
+  
+}
+
+
+
